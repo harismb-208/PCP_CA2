bb322864c25c3d0b1692eaf500566e49bafcc229
Haris Mohamed <harismb208@gmail.com>
2026-04-21 10:08:33 +0530

    final fix login payload correct

--- src/api/api.js
+++ src/api/api.js
@@ -7,19 +7,24 @@ export const fetchToken = async () => {
     const response = await axios.post(
         "https://t4e-testserver.onrender.com/api/login",
         {
-            studentId: "E0123031",
-            set: "B",
+            studentId: "e0123031",
+            set: "setB",
             password: "896201"
         }
     );
 
-    console.log("LOGIN RESPONSE FULL:", JSON.stringify(response.data, null, 2));
+    console.log("LOGIN RESPONSE FULL:", response.data);
 
-    return (
-        response.data.token ||
-        response.data.accessToken ||
-        response.data.data?.token
-    );
+    return response.data.token;
+};
+
+console.log("LOGIN RESPONSE FULL:", JSON.stringify(response.data, null, 2));
+
+return (
+    response.data.token ||
+    response.data.accessToken ||
+    response.data.data?.token
+);
 };
 export const fetchActivities = async (token) => {
     const response = await axios.get(
