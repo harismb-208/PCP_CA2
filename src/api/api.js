92d33ea874e838c4ca9ac7a4e611aac9aad7838b
Haris Mohamed <harismb208@gmail.com>
2026-04-21 09:12:48 +0530

    fix wrong API endpoint

--- src/api/api.js
+++ src/api/api.js
@@ -2,15 +2,15 @@ import axios from "axios";
 
 const BASE_URL = "https://t4e-testserver.onrender.com/api";
 
-// 1. Get Token
+// GET TOKEN
 export const fetchToken = async (password) => {
     const res = await axios.post(`${BASE_URL}/auth`, {
-        password: password
+        password
     });
     return res.data.token;
 };
 
-// 2. Get Private Data
+// GET ACTIVITIES (FIX THIS)
 export const fetchActivities = async (token) => {
     const res = await axios.get(`${BASE_URL}/activities`, {
         headers: {
